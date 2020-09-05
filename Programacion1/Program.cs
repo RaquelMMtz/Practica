7ff1e10e06a103a19a7ef5474ca6c9b02e1fcedb
RaquelMMtz <raquel_maya1202@hotmail.com>
2020-09-05 18:09:43 -0500

    Practica con C#

---
+++ Programacion1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Programacion1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Amigo!");
+        }
+    }
+}
