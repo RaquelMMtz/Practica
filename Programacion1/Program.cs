9c571781ed9e1512ed58782e3f5e186959874d6f
MARCOS JAVIER PALOMO CABRAL <I20050027@monclova.tecnm.mx>
2020-09-08 16:31:34 -0500

    Editado por Marcos Palomo

--- Programacion1/Program.cs
+++ Programacion1/Program.cs
@@ -6,7 +6,7 @@ namespace Programacion1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola Amigos!");
+            Console.WriteLine("Holaa Amigos!");
             Console.ReadKey();
         }
     }
